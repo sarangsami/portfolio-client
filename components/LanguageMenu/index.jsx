0ee7b5cc99c674fbfe9e08912a349edf3b446869
Sarang <sami@novin.dev>
2022-02-01 23:06:34 +0330

    fix: :bug: Eslint warning fixed

--- components/LanguageMenu/index.jsx
+++ components/LanguageMenu/index.jsx
@@ -32,7 +32,10 @@ const StyledMenu = styled((props) => (
         'rgb(55, 65, 81)' :
         theme.palette.grey[300],
     'boxShadow':
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      'rgb(255, 255, 255)' +
+      ' 0px 0px 0px 0px, rgba(0, 0, 0, 0.05)' +
+      ' 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) ' +
+      '0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
     '& .MuiMenu-list': {
       padding: '4px 0',
     },
