2825d2042b0555830f2a27f0a8eb7a4eef7318ce
Sarang Sami <sarangsami75@gmail.com>
2025-11-01 22:57:45 +0330

    fix: :bug: Fix image opt bug

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
-module.exports = {
-  reactStrictMode: true,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: true, // disables the default optimization
+  },
 };
+
+export default nextConfig;
